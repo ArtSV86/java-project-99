1a016c9be94e762d611258dbea2698e9c7a1da39
Art <Art.S.V.86@yandex.ru>
2024-07-15 13:52:33 +0400

    Fix

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -11,8 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AppApplication.class, args);
 	}
+
 	@Bean
 	public static Faker getFaker() {
 		return new Faker();
